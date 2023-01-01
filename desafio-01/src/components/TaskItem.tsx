ef5b94f4abfbe0eaa9be22f9030797b5b92385d9
Leandro Meili <leandro.meili@gmail.com>
2022-12-31 22:46:36 -0300

    finish css

--- desafio-01/src/components/TaskItem.tsx
+++ desafio-01/src/components/TaskItem.tsx
@@ -1,3 +1,6 @@
+import {CheckCircle, Circle, Trash} from "phosphor-react"
+import styles from './TaskItem.module.css'
+
 export interface TaskProps {
     isCompleted: boolean
     text: string
@@ -5,10 +8,44 @@ export interface TaskProps {
 
 
 export function TaskItem({isCompleted, text}: TaskProps) {
+    function taskText() {
+        if (isCompleted) {
+            return styles.completedText
+        } else {
+            return styles.text
+        }
+    }
+
+    function getIcon() {
+        if (isCompleted) {
+            return <>
+                <div className={styles.filledCircle}>
+                    <Circle size={17.45} weight={"fill"}/>
+                </div>
+                <div className={styles.completed}>
+                    <CheckCircle
+                        size={17.45}
+                    />
+                </div>
+            </>;
+        } else {
+            return <>
+                <div className={styles.todoCircle}>
+                    <Circle size={17.45} weight={"bold"}/>
+                </div>
+            </>;
+        }
+    }
+
     return (
-        <div>
-            <div>{`val is ${isCompleted}`}</div>
-            <div>{text}</div>
+        <div className={styles.task}>
+            <div className={styles.icon}>
+                {getIcon()}
+            </div>
+            <div className={taskText()}>{text}</div>
+            <div className={styles.trash}>
+                <Trash size={24}/>
+            </div>
         </div>
     )
 }
